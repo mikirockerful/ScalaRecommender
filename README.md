dbad463a14950de698213d776e10a7c42e9ec63b
mikirockerful <gamallomiguel@gmail.com>
2018-12-01 13:07:29 +0100

    Run from images built in CI

--- README.md
+++ README.md
@@ -19,9 +19,14 @@ Music recommender system built for university course on Big Data infrastructure
 
 # How to run as a spark cluster using HDFS
 
-1 Go to ./compose directory
+1 Login to docker hub (to pull images from private repo)
+```
+docker login
+```
 
-2 Run "prepareAndRun.sh" to download the dataset, build the compose with the individual containers, load dataset files to hdfs and submit the application to the Spark cluster. It takes one optional argument to set the number of spark workers. For example, to run the compose with the master and 3 workers, run:
+2 Go to ./compose directory
+
+3 Run "prepareAndRun.sh" to download the dataset, build the compose with the individual containers, load dataset files to hdfs and submit the application to the Spark cluster. It takes one optional argument to set the number of spark workers. For example, to run the compose with the master and 3 workers, run:
 ```
 prepareAndRun.sh 3
 ```
@@ -29,7 +34,7 @@ The default number of workers is 1.
 
 The Spark master will be accessible through its web UI (http://localhost:8080), as well as Spark workers and both HDFS namenode (http://localhost:50070) and datanode (http://localhost:50075).
 
-3 To run the application more times, after the compose is up:
+4 To run the application more times, after the compose is up:
 ```
 docker exec -it scalarecommender_master_1 spark-submit --master spark://master:7077 /ScalaRecommender/target/scala-2.11/recommender_2.11-2.0.0.jar
 ```
--- compose/prepareAndRun.sh
+++ compose/prepareAndRun.sh
@@ -3,6 +3,7 @@
 #Number of workers to deploy
 workersNumber=1
 
+# Check input arguments
 if [ $# -eq 1 ]; then
 	if [ $1 -eq 0 ]; then
 		echo "0 is not a valid number" >&2; exit 1
@@ -26,26 +27,19 @@ tar -xvzf profiledata_06-May-2005.tar.gz
 mv profiledata_06-May-2005 datasetFiles
 # Take only the first 10000 records
 head -n 10000 ./datasetFiles/user_artist_data.txt > ./datasetFiles/user_artist_data_10000.txt
-# Copy the dataset files to Dockerfiles' context
-cp -r datasetFiles ./master && cp -r datasetFiles ./worker
-# Build it
-docker-compose build
-# Run it in background
+# Run the containers
 docker-compose up -d --scale worker=$workersNumber
 # Wait 10 seconds - namenode needs some time to start running
 echo "Waiting for 10 seconds..."
 sleep 10
 # Copy the directory with dataset files from host to cointainer
 docker cp ./datasetFiles/ namenode:/
+# Disable HDFS safe mode
+docker exec namenode hadoop dfsadmin -safemode leave
 # Copy from container's local fs to HDFS
 docker exec namenode hadoop fs -copyFromLocal /datasetFiles /
-# Remove datasetFiles from container's local fs
-#docker exec namenode rm -r /datasetFiles
 # Remove the datasetFiles
 rm profiledata_06-May-2005.tar.gz
 rm -r datasetFiles
-# These 2 commands are not needed when working with hdfs
-#rm -r ./master/datasetFiles
-#rm -r ./worker/datasetFiles
 # Execute the recommender
 docker exec -it scalarecommender_master_1 spark-submit --master spark://master:7077 /ScalaRecommender/target/scala-2.11/recommender_2.11-2.0.0.jar
